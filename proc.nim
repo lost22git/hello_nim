d099f7f92626611b1e70c2ce95b55f2534f9f1ac
lost22git <lost22git>
2023-12-17 12:21:45 +0800

    first commit

---
+++ proc.nim
@@ -0,0 +1,71 @@
+import std/[strformat]
+
+#[
+参数传递类型：
+* 值传递
+* 引用传递
+]#
+
+proc `===`(title: string) =
+  echo fmt"--{title:-<30}"
+
+type Fighter = object
+  name: string = "Ken"
+  skill: seq[string]
+
+proc testImmut(v: Fighter) =
+  # v.skill.add "hadouken" # 不允许修改
+  echo fmt"testImmut: {v.repr =}"
+
+proc testMut(v: var Fighter) =
+  v.skill.add "hadouken"
+  echo fmt"testMut: {v.repr =}"
+
+proc testCopy(v: sink Fighter) =
+  v.skill.add "hadouken"
+  echo fmt"testCopy: {v.repr =}"
+
+proc testRef(v: ref Fighter) =
+  v.skill.add "hadouken"
+  echo fmt"testRef: {v.repr =}"
+
+proc testPtr(v: ptr Fighter) =
+  v[].skill.add "hadouken"
+  echo fmt"testPtr: {v.repr =}"
+
+==="testImmut"
+
+let toImmut = Fighter()
+echo fmt"{toImmut.repr =}"
+toImmut.testImmut
+echo fmt"{toImmut.repr =}"
+
+==="testMut"
+
+var toMut = Fighter()
+echo fmt"{toMut.repr =}"
+toMut.testMut
+echo fmt"{toMut.repr =}"
+
+==="testCopy"
+
+let toCopy = Fighter()
+echo fmt"{toCopy.repr =}"
+toCopy.testCopy
+echo fmt"{toCopy.repr =}"
+
+==="testRef"
+
+let toRef = Fighter.new()
+echo fmt"{toRef.repr =}"
+toRef.testRef
+echo fmt"{toRef.repr =}"
+
+==="testPtr"
+
+let
+  a = Fighter()
+  toPtr = a.addr
+echo fmt"{toPtr.repr =}"
+toPtr.testPtr
+echo fmt"{toPtr.repr =}"
---
+++ string.nim
@@ -0,0 +1,101 @@
+import std/[unicode, strutils, strformat]
+
+proc `===`(title: string) =
+  echo fmt"--{title:-<30}"
+
+==="字符串连接"
+
+echo "你好" & "，" & "世界"
+
+==="字符串格式化"
+
+let str = "世界"
+
+echo "你好，$1" % str
+
+echo fmt"你好，{str}"
+
+echo fmt("你好，<str>", '<', '>')
+
+==="字符串表示"
+
+let v = @["你好", "世界"]
+echo $v
+echo fmt"debug: {v = }"
+echo fmt"repr: { v.repr }"
+
+==="多行字符串"
+
+echo """
+你好
+世界"""
+
+==="raw 字符串"
+
+echo r"D:\learning\nim"
+
+==="动态字符串"
+
+var sb: string = "你好"
+
+sb.add "，"
+sb.add "世界"
+
+echo sb
+
+==="字符串长度"
+
+echo fmt"`{sb}` 字节长度: {sb.len}"
+
+echo fmt"`{sb}` rune长度: {sb.runeLen}"
+
+==="字符串 sub string"
+
+var
+  substrOrigin = "你好"
+  substr = substrOrigin[0 ..^ 1] # 复制指定范围的字符到新字符串
+
+echo fmt"{substrOrigin = }"
+echo fmt"{substr = }"
+assert substrOrigin == substr
+
+substrOrigin[0 ..^ 1] = "世界" # 修改原来的字符串
+
+echo fmt"{substrOrigin = }"
+echo fmt"{substr = }"
+assert substrOrigin != substr
+
+==="字符串解析"
+
+import std/[nre]
+
+var csv =
+  """
+nim,gc,native code
+rust,no gc,native code
+java,gc,bytecode
+"""
+
+when not defined(windows):
+  ==="正则表达式 pcre"
+  let
+    pattern = re"(.+),(.+),(.+)\r?\n"
+    captureCount = pattern.captureCount
+
+  for m in csv.findIter(pattern):
+    echo "=".repeat 30
+    let
+      captures = m.captures
+      captureBounds = m.captureBounds
+    for i in 0 ..< captureCount:
+      let
+        cap = captures[i]
+        capBound = captureBounds[i]
+      echo fmt"[{i}]: {capBound} -> {cap}"
+else:
+  ==="strscans"
+  import std/[strscans]
+  var language, mm, compilationProduct: string
+  for line in csv.splitLines:
+    if line.scanf("$w,$w,$w", language, mm, compilationProduct):
+      echo fmt"{language = }, {mm = }, {compilationProduct = }"
